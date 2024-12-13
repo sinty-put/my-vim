634fc736500bf34e14e14056f95b3328537fccaa
rdl-agen46 <naufalulwan63@gmail.com>
2024-12-13 16:46:39 +0700

    ish ish

--- lua/scode/plugins/fluttertools.lua
+++ lua/scode/plugins/fluttertools.lua
@@ -43,6 +43,23 @@ return {
           require("dap.ext.vscode").load_launchjs()
         end,
       },
+      dev_log = {
+        enabled = true,
+        filter = nil, -- optional callback to filter the log
+        -- takes a log_line as string argument; returns a boolean or nil;
+        -- the log_line is only added to the output if the function returns true
+        notify_errors = false, -- if there is an error whilst running then notify the user
+        open_cmd = "15split", -- command to use to open the log buffer
+        focus_on_open = true, -- focus on the newly opened log window
+      },
+      dev_tools = {
+        autostart = false, -- autostart devtools server if not detected
+        auto_open_browser = false, -- Automatically opens devtools in the browser
+      },
+      outline = {
+        open_cmd = "30vnew", -- command to use to open the outline buffer
+        auto_open = false, -- if true this will open the outline automatically when it is first populated
+      },
     })
   end,
 }
