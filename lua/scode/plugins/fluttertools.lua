99ab7f4282f7e4557593c72a326fa2adc16bb134
rdl-agen46 <naufalulwan63@gmail.com>
2024-12-13 16:55:05 +0700

    ahahaha

--- lua/scode/plugins/fluttertools.lua
+++ lua/scode/plugins/fluttertools.lua
@@ -44,21 +44,14 @@ return {
         end,
       },
       dev_log = {
-        enabled = true,
-        filter = nil, -- optional callback to filter the log
-        -- takes a log_line as string argument; returns a boolean or nil;
-        -- the log_line is only added to the output if the function returns true
-        notify_errors = false, -- if there is an error whilst running then notify the user
-        open_cmd = "15split", -- command to use to open the log buffer
-        focus_on_open = true, -- focus on the newly opened log window
+        enabled = false,
+        open_cmd = "tabedit",
+        focus_on_open = true,
       },
-      dev_tools = {
-        autostart = false, -- autostart devtools server if not detected
-        auto_open_browser = false, -- Automatically opens devtools in the browser
-      },
+
       outline = {
-        open_cmd = "30vnew", -- command to use to open the outline buffer
-        auto_open = false, -- if true this will open the outline automatically when it is first populated
+        open_cmd = "30vnew",
+        auto_open = false,
       },
     })
   end,
