5c36f79bee322ed875a54500feece584d11f9804
rdl-agen46 <822157@hq.bni.co.id>
2025-01-30 10:29:04 +0700

    tambahan

--- lua/scode/plugins/tabnine.lua
+++ lua/scode/plugins/tabnine.lua
@@ -1,4 +1,16 @@
 return {
   "codota/tabnine-nvim",
-  build = "./dl_binaries.sh",
+  build = "C:\\Users\\822157\\Documents\\PowerShell\\dl_binaries.ps1",
+  config = function()
+    require("tabnine").setup({
+      disable_auto_comment = true,
+      accept_keymap = "<Tab>",
+      dismiss_keymap = "<C-]>",
+      debounce_ms = 800,
+      suggestion_color = { gui = "#808080", cterm = 244 },
+      exclude_filetypes = { "TelescopePrompt", "NvimTree" },
+      log_file_path = nil,
+      ignore_certificate_errors = false,
+    })
+  end,
 }
