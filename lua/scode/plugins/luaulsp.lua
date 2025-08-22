542e38709b0b10cc4cc8a91cdee2f6bba1ba3589
wdl-idp <wdl-idp>
2025-08-22 16:00:16 +0700

    Settings luau

--- lua/scode/plugins/luaulsp.lua
+++ lua/scode/plugins/luaulsp.lua
@@ -3,4 +3,22 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
+  opts = {
+    platform = {
+      type = "roblox",
+    },
+    types = {
+      roblox_security_level = "PluginSecurity",
+    },
+    plugin = {
+      enable = true,
+      port = 3667,
+    },
+    sourcemap = {
+      enable = true,
+      autogenerate = true,
+      rojo_project_file = "default.project.json",
+      sourcemap_file = "sourcemap.json",
+    },
+  },
 }
